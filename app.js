d80e4752b7d404194a0ef365eed580491610163a
Jeroen Walta <jeroenwalta@gmail.com>
2019-12-17 12:44:40 +0100

    bug fix

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ requestKMD(requestStringKMD, (error, response, body) => {
       console.log(transActUtxos);
       console.log(`amount to spend: ${utxoBalance}. KMDbalance: ${KMDbalance}`)
       // create RawTransactionString
-      const RawTransactionString = `~/komodo/src/komodo-cli createrawtransaction '${transActUtxos}' 'rainTransactions'`;
+      const RawTransactionString = `~/komodo/src/komodo-cli createrawtransaction '${JSON.stringify(transActUtxos)}' '${rainTransactions}'`;
       console.log(RawTransactionString);
 
     });
