17570073beac546e0181b395361af48134588b00
jippey67 <jeroenwalta@gmail.com>
2020-03-11 21:13:17 +0100

    bug fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const marketMaker = require('./allToKMD/marketMaker')
+const marketMaker = require('./marketMaker')
 const fs = require('fs')
 var ourCoins = JSON.parse(fs.readFileSync('./ourCoins', 'utf8'))
 const {millisToNoon, millisToMidnight, millisTo22pm, millisTo23pm} = require('./timing')
