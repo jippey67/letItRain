d9a5bc445733e8d5b1ba75e4d24e909dfc5ccdc0
jippey67 <jeroenwalta@gmail.com>
2020-03-11 17:03:23 +0100

    bug fix

--- test2.js
+++ test2.js
@@ -2,4 +2,11 @@ const marketMaker = require('./allToKMD/marketMaker')
 const fs = require('fs')
 var ourCoins = JSON.parse(fs.readFileSync('./ourCoins', 'utf8'))
 
-marketMaker.placeOrders(ourCoins)
+
+ourCoins.forEach(coin => {
+  marketMaker.connectCoin(coin)
+})
+
+setTimeout(() => {
+  marketMaker.placeOrders(ourCoins)
+}, 5000)
