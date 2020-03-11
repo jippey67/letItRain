246879150761d39cba6129dd47bc512429168152
Jeroen Walta <jeroenwalta@gmail.com>
2020-03-11 09:19:26 +0100

    code cleanup

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const {millisToNoon, millisToMidnight} = require('./timing')
+var fs = require('fs');
+const CoinpaprikaAPI = require('@coinpaprika/api-nodejs-client');
+const client = new CoinpaprikaAPI();
+var src = fs.readFileSync('./allToKMD/ourCoins')
+const marketMaker = require('./allToKMD/marketMaker')
+
+
+const {millisToNoon, millisToMidnight, millisTo22pm, illisTo23pm} = require('./timing')
 const {distribute} = require('./distributeKMD')
 
 const rain = () => {
+  // distribute available KMD to CCL holders
   setTimeout(() => {
-    distribute() // wait to midnight for first distrubution
+    distribute()
     setInterval(() => {
       distribute()
     }, 24*60*60*1000) // wait 24 hours after first attempt
-  }, millisToMidnight) // first attempt at next midnight
+  }, millisToMidnight) // first attempt at midnight
+
+  // sends all collected KMD to the dsitributor address
+  setTimeout(() => {
+    marketMaker.sendKMDtoDistributor(ourCoins[0])
+    setInterval(() => {
+      marketMaker.sendKMDtoDistributor(ourCoins[0])
+    }, 24*60*60*1000) // wait 24 hours after first attempt
+  }, millisTo23pm) // first attempt at 23pm
 }
 
-rain()
+
+
+setTimeout(() => { //allow for 60 secs for connecting to all ourCoins
+  rain()
+},60000)
--- timing.js
+++ timing.js
@@ -9,3 +9,15 @@ if (currentUTCHour > 11) {
   nextNoon.setDate(new Date().getDate() + 1)
 }
 exports.millisToNoon = nextNoon.setUTCHours(12,0,0,0) - now
+
+const next23pm = new Date()
+if (currentUTCHour > 22) {
+  nextNoon.setDate(new Date().getDate() + 1)
+}
+exports.millisTo23pm = nextNoon.setUTCHours(23,0,0,0) - now
+
+const next22pm = new Date()
+if (currentUTCHour > 21) {
+  nextNoon.setDate(new Date().getDate() + 1)
+}
+exports.millisTo22pm = nextNoon.setUTCHours(22,0,0,0) - now
