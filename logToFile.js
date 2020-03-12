bda388821c333aa5c407421af931d6c781ab19db
jippey67 <jeroenwalta@gmail.com>
2020-03-12 23:34:23 +0100

    klein presentatiefixje in de logfile

--- logToFile.js
+++ logToFile.js
@@ -6,9 +6,9 @@ module.exports.log = (regel) => {
   let date = ("0" + now.getUTCDate()).slice(-2);
   let month = ("0" + (now.getUTCMonth() + 1)).slice(-2);
   let year = now.getUTCFullYear();
-  let hours = now.getUTCHours();
-  let minutes = now.getUTCMinutes();
-  let seconds = now.getUTCSeconds();
+  let hours = ("0" + now.getUTCHours()).slice(-2);
+  let minutes = ("0" + now.getUTCMinutes()).slice(-2);
+  let seconds = ("0" + now.getUTCSeconds()).slice(-2);
 
   const filename = "./logs/" + year + "-" + month + "-" + date +'.log'
   fs.appendFile(filename, hours + ":" + minutes + ":" + seconds + ' ' + regel+'\n', function (err) {
