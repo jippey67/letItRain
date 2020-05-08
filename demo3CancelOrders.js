d1cf92d2bab0342537366d934e1afc219e95170d
Jeroen Walta <jeroenwalta@Jeroens-iMac.local>
2020-05-08 15:21:42 +0200

    added demo scripts

---
+++ demo3CancelOrders.js
@@ -0,0 +1,9 @@
+
+const marketMaker = require('./marketMaker')
+const fs = require('fs')
+var ourCoins = JSON.parse(fs.readFileSync('./ourCoins', 'utf8'))
+const {distribute} = require('./distributeKMD')
+const ltf = require('./logToFile')
+
+
+marketMaker.cancelOrders()
---
+++ demo4SendKMDtoDistrubutor.js
@@ -0,0 +1,8 @@
+
+const marketMaker = require('./marketMaker')
+const fs = require('fs')
+var ourCoins = JSON.parse(fs.readFileSync('./ourCoins', 'utf8'))
+const {distribute} = require('./distributeKMD')
+const ltf = require('./logToFile')
+
+marketMaker.sendKMDtoDistributor(ourCoins[0])
